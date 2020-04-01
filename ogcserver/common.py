207fdd6df9e7054961989dea1799da722c802ee4
Clemens Rudert <clemens.rudert@posteo.de>
2020-04-01 12:59:04 +0200

    fix depricated minzoom maxzoom issue

--- ogcserver/common.py
+++ ogcserver/common.py
@@ -295,8 +295,8 @@ def copy_layer(obj):
     if hasattr(obj, 'toleranceunits'):
         lyr.toleranceunits = obj.toleranceunits
     lyr.srs = obj.srs
-    lyr.minimum-scale-denominator = obj.minimum-scale-denominator
-    lyr.maximum-scale-denominator = obj.maximum-scale-denominator
+    lyr.minzoom = obj.minimum-scale-denominator
+    lyr.maxzoom = obj.maximum-scale-denominator
     lyr.active = obj.active
     lyr.queryable = obj.queryable    
     lyr.clear_label_cache = obj.clear_label_cache
